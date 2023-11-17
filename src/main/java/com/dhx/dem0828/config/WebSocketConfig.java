7d00a9e87ce93b1ef90eceac703832fa2dd3b58e
adorabled4 <dhx2648466390@163.com>
2023-11-17 14:17:43 +0800

    feat: websocket-server demo

---
+++ src/main/java/com/dhx/dem0828/config/WebSocketConfig.java
@@ -0,0 +1,19 @@
+package com.dhx.dem0828.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+
+/**
+ * @author adorabled4
+ * @className WebSocketConfig
+ * @date : 2023/11/17/ 13:58
+ **/
+@Configuration
+public class WebSocketConfig {
+
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter() {
+        return new ServerEndpointExporter();
+    }
+}
--- src/main/java/com/dhx/dem0828/controller/TestController.java
+++ src/main/java/com/dhx/dem0828/controller/TestController.java
@@ -1,11 +1,17 @@
 package com.dhx.dem0828.controller;
 
+import com.dhx.dem0828.manager.WebSocketServer;
 import com.dhx.dem0828.mq.MessageProducer;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
 
 /**
  * @author adorabled4
@@ -19,10 +25,23 @@ public class TestController {
     @Resource
     MessageProducer messageProducer;
 
+    @Resource
+    WebSocketServer webSocketServer;
+
     @GetMapping("/test/send")
     public String send() {
         long userId = 45376384L;
         messageProducer.sendGenChartMessage(String.valueOf(userId));
         return "send successfully";
+    }
+
+    @GetMapping("/test/send/ws/{userId}")
+    public String send2WS(@PathVariable long userId){
+        try {
+            webSocketServer.sendMessage("this is a message from server!", new HashSet<>(Arrays.asList(String.valueOf(userId))));
+            return "success";
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
